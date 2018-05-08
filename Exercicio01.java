daa0cb73f99c4e8908ff3208926a61fdacfb050b
Gabriel de Oliveira Ferreira <38558405+gabrielferreirah@users.noreply.github.com>
2018-05-08 09:17:12 -0300

    CEDUP - 08/05

--- Exercicio01.java
+++ Exercicio01.java
@@ -1,5 +1,19 @@
-public class Exercicio01{
-    public static void main(String[] args) {
-        
-    }
+import javax.swing.JOptionPane;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+
+		int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o numero que deseja"));
+		String texto = "";
+
+		for (int i = 0; i <= 10; i++) {
+			for (int j = 0; j < 1; j++) {
+				texto += (i + " x " + numero + " = " + (i * numero));
+			}
+			texto = texto + "\n";
+		}
+		JOptionPane.showMessageDialog(null, texto);
+	}
+
 }

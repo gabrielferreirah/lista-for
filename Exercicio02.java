d87122633cfa51fec1fc517eb2112284a295f286
Gabriel de Oliveira Ferreira <38558405+gabrielferreirah@users.noreply.github.com>
2018-05-10 08:04:31 -0300

    CEDUP - 08/05

---
+++ Exercicio02.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		
+		String texto = "";
+		
+		for(int j = 0; j <= 10; j++){
+			for(int i = 0; i <= 10; i++){
+				texto += i + " x " + j + " = " + i*j + "     ";
+			}
+			texto += "\n";
+		}
+		JOptionPane.showMessageDialog(null, texto);
+	}
+
+}
